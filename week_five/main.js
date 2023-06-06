948a88b5ba2ead52dbdf57e585a57aee5b73bfda
John Doe <betiaman@example.com>
2023-06-06 11:29:39 -0500

    adds js files

---
+++ week_five/main.js
@@ -0,0 +1,10 @@
+export function add(x, y) {
+        return x + y;
+    }
+    
+    export function sub(x, y) {
+        return x - y;
+    }
+    
+    export default function greeting(name) {
+    }  
